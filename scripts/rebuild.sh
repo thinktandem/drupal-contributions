ed561a6d12148eb6f5cc52b86d55b6a8a4ce9da5
Benji Fisher <benji@FisherFam.org>
2020-07-04 17:15:38 -0400

    Add a Lando command to run phpunit.
    For example:
    
    lando phpunit web/core/modules/migrate_drupal_ui/tests/src/Functional/d7/IdConflictTest.php
    
    For some reason, .../d7/*.php runs only one of the three tests in that
    directory.

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -2,6 +2,7 @@
 
 drush --root=/app/web si --db-url=mysql://drupal8:drupal8@database/drupal8 -y
 drush --root=/app/web en simpletest -y
+mkdir -p -m 777 /app/web/sites/simpletest/browser_output
 find /app/web/sites/default -type d -exec chmod 777 '{}' \;
 drush --root=/app/web --uri=https://drupal-contributions.lndo.site uli
 echo "vendor" >> /app/web/.gitignore

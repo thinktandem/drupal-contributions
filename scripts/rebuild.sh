696dedc35774af8e07c490578e9cc292dc6315cf
Benji Fisher <benji@FisherFam.org>
2020-07-04 17:10:01 -0400

    Make directories, not files, executable on rebuild.

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -2,7 +2,7 @@
 
 drush --root=/app/web si --db-url=mysql://drupal8:drupal8@database/drupal8 -y
 drush --root=/app/web en simpletest -y
-chmod -R 777 /app/web/sites/default
+find /app/web/sites/default -type d -exec chmod 777 '{}' \;
 drush --root=/app/web --uri=https://drupal-contributions.lndo.site uli
 echo "vendor" >> /app/web/.gitignore
 echo ".gitignore\nsites/simpletest" >> /app/web/.gitignore

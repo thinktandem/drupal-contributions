dd1079aeebcd94204a91dcfa2250d74571413174
gitressa <3491208+gitressa@users.noreply.github.com>
2021-05-17 11:46:06 +0200

    Removes SimpleTest

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 /app/web/vendor/drush/drush/drush --root=/app/web si --db-url=mysql://drupal9:drupal9@database/drupal9 -y
-/app/web/vendor/drush/drush/drush --root=/app/web en simpletest -y
 mkdir -p -m 777 /app/web/sites/simpletest/browser_output
 find /app/web/sites/default -type d -exec chmod 777 '{}' \;
 /app/web/vendor/drush/drush/drush --root=/app/web --uri=https://drupal-contributions.lndo.site uli

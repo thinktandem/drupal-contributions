3a626c1bb55f3ac6c5353d73c9401bfbcdc069d5
John Ouellet <john@thinktandem.io>
2022-03-09 09:20:25 -0500

    Adjust more d10 items

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/app/web/vendor/drush/drush/drush --root=/app/web si --db-url=mysql://drupal9:drupal9@database/drupal9 -y
+/app/web/vendor/drush/drush/drush --root=/app/web si --db-url=mysql://drupal10:drupal10@database/drupal10 -y
 mkdir -p -m 777 /app/web/sites/simpletest/browser_output
 find /app/web/sites/default -type d -exec chmod 777 '{}' \;
 /app/web/vendor/drush/drush/drush --root=/app/web --uri=https://drupal-contributions.lndo.site uli
--- scripts/site-install.sh
+++ scripts/site-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/app/web/vendor/drush/drush/drush --root=/app/web si --db-url=mysql://drupal9:drupal9@database/drupal9 -y $1
+/app/web/vendor/drush/drush/drush --root=/app/web si --db-url=mysql://drupal10:drupal10@database/drupal10 -y $1
 find /app/web/sites/default -type d -exec chmod 777 '{}' \;
 /app/web/vendor/drush/drush/drush --root=/app/web --uri=https://drupal-contributions.lndo.site uli
